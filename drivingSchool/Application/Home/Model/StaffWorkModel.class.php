5dae33322e9858386f8203117d54688d2fabeb8c
hcw19930623 <472368077@qq.com>
2016-04-13 19:10:29 +0800

    hechengwei

---
+++ drivingSchool/Application/Home/Model/StaffWorkModel.class.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * @author:hechengwei
+ * @date  :2016-4-13
+ * @tablename:员工考勤表
+ * */
+namespace Home\Model;
+use Think\Model;
+class StaffWorkModel extends Model {
+    /*
+     * 查询数据
+     * @$where  条件
+     * @$order  排序字段
+     * @$limit  限制几条数据
+     * */
+    public function getValue($where,$order,$limit)
+    {
+        return $this->where($where)->order("$order")->limit($limit)->find();
+    }
+    /*
+     * 删除数据
+     *@$where   条件
+     * */
+    public function delValue($where)
+    {
+        return $this->where($where)->delete();
+    }
+}
+?>
