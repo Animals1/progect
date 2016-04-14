476fc50e6fba2bea9cff45b9ace1e659b30262d6
xueyunhuan <957232222@qq.com>
2016-04-14 08:30:43 +0800

    xueyunhuan

--- drivingSchool/admin/Home/Model/CarStatusModel.class.php
+++ drivingSchool/admin/Home/Model/CarStatusModel.class.php
@@ -1,8 +1,36 @@
 <?php
+/*
+ * @author:xueyunhuan
+ * @date  :2016-4-13
+ * @tablename:车辆类型表
+ * */
 namespace Home\Model;
 use Think\Model;
-class CarStatusModel extends Model {
-
-
-
+class CarTypeModel extends Model {
+	/*
+	 *显示类型
+	*/
+		public function show(){
+			return $this->select();
+		}
+		/*
+		 * 增加类型
+		*/
+		public function add_cartype($type_name){
+			$data['type_name'] = $type_name;
+			return $this->add($data);
+		}
+		/*
+		 * 修改类型
+		*/
+		public function update_cartype($type_id,$type_name){
+			$data['type_id'] = $type_id;
+			return $this->where("type_id = $type_id")->save();
+		}
+		/*
+		 * 删除类型
+		*/
+		public function delete_cartype($type_id){
+			return $this->where("type_id = $type_id")->delete();
+		}
 }
