61780e950db2a8f8094b614c191f9de76c4f7e02
xueyunhuan <957232222@qq.com>
2016-04-15 13:10:05 +0800

    xueyunhuan

--- drivingSchool/admin/Runtime/Cache/Home/f869cb2ca0001340f2421db3cac53285.php
+++ drivingSchool/admin/Runtime/Cache/Home/f869cb2ca0001340f2421db3cac53285.php
@@ -31,7 +31,7 @@
 <div class="logintop">    
     <span>欢迎登录后台管理界面平台</span>    
     <ul>
-    <li><a href="/progect/drivingSchool/index.php/Home/Index/index">回首页</a></li>
+    <li><a href="/index.php/Home/Index/index">回首页</a></li>
     <li><a href="#">帮助</a></li>
     <li><a href="#">关于</a></li>
     </ul>    
