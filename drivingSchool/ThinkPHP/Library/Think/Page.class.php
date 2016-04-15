aec925f9aac8cffab2a982cdb883de5637e8b534
suixin <15652456348@163.com>
2016-04-15 13:01:13 +0800

    yaobowen

--- drivingSchool/ThinkPHP/Library/Think/Page.class.php
+++ drivingSchool/ThinkPHP/Library/Think/Page.class.php
@@ -26,9 +26,9 @@ class Page{
 	// 分页显示定制
     private $config  = array(
         'header' => '<span class="rows">共 %TOTAL_ROW% 条记录</span>',
-        'prev'   => '<<',
-        'next'   => '>>',
-        'first'  => '1...',
+        'prev'   => '上一页',
+        'next'   => '下一页',
+        'first'  => '首页',
         'last'   => '...%TOTAL_PAGE%',
         'theme'  => '%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END%',
     );
