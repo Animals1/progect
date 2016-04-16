52090ed979728e68a47bc4a8575a696dcc217e85
xueyunhuan <957232222@qq.com>
2016-04-16 08:03:55 +0800

    xueyunhuan

--- drivingSchool/admin/Runtime/Cache/Home/1a4c7133ffffca5d3bba516c4560d8e9.php
+++ drivingSchool/admin/Runtime/Cache/Home/1a4c7133ffffca5d3bba516c4560d8e9.php
@@ -2,17 +2,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<<<<<<< HEAD
-<title>欢迎登录后台管理系统</title>
-<link href="/progect/drivingSchool/Public/admin/css/style.css" rel="stylesheet" type="text/css" />
-<script language="JavaScript" src="/progect/drivingSchool/Public/admin/js/jquery.js"></script>
-<script src="/progect/drivingSchool/Public/admin/js/cloud.js" type="text/javascript"></script>
-=======
 <title>学员登录</title>
 <link href="/Public/admin/css/style.css" rel="stylesheet" type="text/css" />
 <script language="JavaScript" src="/Public/admin/js/jquery.js"></script>
 <script src="/Public/admin/js/cloud.js" type="text/javascript"></script>
->>>>>>> ab93db8c4618d01ee74c960c40b9146db003154a
 
 <script language="javascript">
 	$(function(){
@@ -25,11 +18,7 @@
 
 </head>
 
-<<<<<<< HEAD
-<body style="background-color:#df7611; background-image:url(/progect/drivingSchool/Public/admin/images/light.png); background-repeat:no-repeat; background-position:center top; overflow:hidden;">
-=======
 <body style="background-color:#df7611; background-image:url(/Public/admin/images/light.png); background-repeat:no-repeat; background-position:center top; overflow:hidden;">
->>>>>>> ab93db8c4618d01ee74c960c40b9146db003154a
 
 
 
@@ -40,15 +29,9 @@
 
 
 <div class="logintop">    
-<<<<<<< HEAD
-    <span>欢迎登录后台管理界面平台</span>    
-    <ul>
-    <li><a href="/progect/drivingSchool/index.php/Home/Index/index">回首页</a></li>
-=======
     <span>欢迎使用-驾校管理系统-学员登录平台</span>    
     <ul>
     <li><a href="/index.php/Home/Login/index">回首页</a></li>
->>>>>>> ab93db8c4618d01ee74c960c40b9146db003154a
     <li><a href="#">帮助</a></li>
     <li><a href="#">关于</a></li>
     </ul>    
@@ -66,16 +49,12 @@
     <li class="yzm">
     <span><input name="" type="text" value="验证码" onclick="JavaScript:this.value=''"/></span><cite>X3D5S</cite> 
     </li>
-<<<<<<< HEAD
-    <li><input name="" type="button" class="loginbtn" value="登录"  onclick="javascript:window.location='main.html'"  /><label><input name="" type="checkbox" value="" checked="checked" />记住密码</label><label><a href="#">忘记密码？</a></label></li>
-=======
     <li><input name="" type="button" class="loginbtn" value="登录"  onclick="javascript:window.location='main.html'"  />
     <label>
     <input type="checkbox" name="checkbox" id="checkbox" value="7" />
     记住密码</label><label><a href="#">忘记密码？</a>
     </label>
     </li>
->>>>>>> ab93db8c4618d01ee74c960c40b9146db003154a
     </ul>
     
     
@@ -84,11 +63,7 @@
     </div>
     
     
-<<<<<<< HEAD
-    <div class="loginbm">版权所有  2014  <a href="http://www.uimaker.com">uimaker.com</a>  仅供学习交流，勿用于任何商业用途</div>
-=======
     
->>>>>>> ab93db8c4618d01ee74c960c40b9146db003154a
 	
     
 </body>
--- drivingSchool/admin/Runtime/Cache/Home/4e9f4091e6d48ca90fafa6306b883b29.php
+++ drivingSchool/admin/Runtime/Cache/Home/4e9f4091e6d48ca90fafa6306b883b29.php
@@ -2,21 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<<<<<<< HEAD
-<title>信息管理系统-uimaker.com</title>
-<link href="/Public/admin/css/style.css" rel="stylesheet" type="text/css" />
-<script language="JavaScript" src="/Public/admin/js/jquery.js"></script>
-=======
-<<<<<<< HEAD
-<title>信息管理系统-uimaker.com</title>
-<link href="/progect/drivingSchool/Public/admin/css/style.css" rel="stylesheet" type="text/css" />
-<script language="JavaScript" src="/progect/drivingSchool/Public/admin/js/jquery.js"></script>
-=======
 <title>驾校-信息管理系统</title>
 <link href="/Public/admin/css/style.css" rel="stylesheet" type="text/css" />
 <script language="JavaScript" src="/Public/admin/js/jquery.js"></script>
->>>>>>> ab93db8c4618d01ee74c960c40b9146db003154a
->>>>>>> c04be83576cf56e93ebbb3cbc5b85bc716b362cd
 
 <script language="javascript">
 	$(function(){
@@ -29,29 +17,13 @@
 
 </head>
 
-<<<<<<< HEAD
 <body style="background-color:#df7611; background-image:url(/Public/admin/images/light1.png); background-repeat:no-repeat; background-position:center top; overflow:hidden;">
-=======
-<<<<<<< HEAD
-<body style="background-color:#df7611; background-image:url(/progect/drivingSchool/Public/admin/images/light1.png); background-repeat:no-repeat; background-position:center top; overflow:hidden;">
-=======
-<body style="background-color:#df7611; background-image:url(/Public/admin/images/light1.png); background-repeat:no-repeat; background-position:center top; overflow:hidden;">
->>>>>>> ab93db8c4618d01ee74c960c40b9146db003154a
->>>>>>> c04be83576cf56e93ebbb3cbc5b85bc716b362cd
 
 
 <div class="logintop">    
     <span>欢迎进入驾校管理系统</span>    
     <ul>
-<<<<<<< HEAD
-    <li><a href="/index.php/Home/Index/index">回首页</a></li>
-=======
-<<<<<<< HEAD
-    <li><a href="/progect/drivingSchool/index.php/Home/Index/index">回首页</a></li>
-=======
     <li><a href="/index.php/Home/Login/index">回首页</a></li>
->>>>>>> ab93db8c4618d01ee74c960c40b9146db003154a
->>>>>>> c04be83576cf56e93ebbb3cbc5b85bc716b362cd
     <li><a href="#">帮助</a></li>
     <li><a href="#">关于</a></li>
     </ul>    
@@ -64,18 +36,8 @@
     <div class="loginbox0">
     
     <ul class="loginlist" style="margin-left:200px">
-<<<<<<< HEAD
     <li><a href="/index.php/Home/login/adminlogin"><img src="/Public/admin/images/l01.png"  alt="电子监察系统"/><p>管理员<br />登录</p></a></li>
     <li><a href="/index.php/Home/login/stulogin"><img src="/Public/admin/images/l02.png"  alt="电子监察系统"/><p>学员登陆<br /></p></a></li>
-=======
-<<<<<<< HEAD
-    <li><a href="/progect/drivingSchool/index.php/Home/login/adminlogin"><img src="/progect/drivingSchool/Public/admin/images/l01.png"  alt="电子监察系统"/><p>管理员<br />登录</p></a></li>
-    <li><a href="/progect/drivingSchool/index.php/Home/login/stulogin"><img src="/progect/drivingSchool/Public/admin/images/l02.png"  alt="电子监察系统"/><p>学员登陆<br /></p></a></li>
-=======
-    <li><a href="/index.php/Home/login/adminlogin"><img src="/Public/admin/images/l01.png"  alt="电子监察系统"/><p>管理员<br />登录</p></a></li>
-    <li><a href="/index.php/Home/login/stulogin"><img src="/Public/admin/images/l02.png"  alt="电子监察系统"/><p>学员登陆<br /></p></a></li>
->>>>>>> ab93db8c4618d01ee74c960c40b9146db003154a
->>>>>>> c04be83576cf56e93ebbb3cbc5b85bc716b362cd
 
     </ul>
     
@@ -84,15 +46,7 @@
     
     </div>
     
-<<<<<<< HEAD
-    <div class="loginbm">版权所有  2014&nbsp;&nbsp;&nbsp; <strong>uimaker.com</strong> &nbsp;&nbsp;&nbsp;仅供学习交流，勿用于任何商业用途</div>
-=======
-<<<<<<< HEAD
-    <div class="loginbm">版权所有  2014&nbsp;&nbsp;&nbsp; <strong>uimaker.com</strong> &nbsp;&nbsp;&nbsp;仅供学习交流，勿用于任何商业用途</div>
-=======
    
->>>>>>> ab93db8c4618d01ee74c960c40b9146db003154a
->>>>>>> c04be83576cf56e93ebbb3cbc5b85bc716b362cd
 	
     
 </body>
