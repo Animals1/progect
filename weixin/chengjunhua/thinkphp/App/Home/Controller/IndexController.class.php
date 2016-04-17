e4e376c3e1bc7fdd415da297ba88751f5cd3d9b4
cjh <664396185@qq.com>
2016-04-17 21:12:51 +0800

    chengjunhua

---
+++ weixin/chengjunhua/thinkphp/App/Home/Controller/IndexController.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+*微信开发---手机官网
+*@author：chengjunhua
+*/
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+    	$this->display('index');
+       }
+}
