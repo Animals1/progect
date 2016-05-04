760e0351158c569118d1a07e488b51b2e3b748f3
Animals1 <1031894622@qq.com>
2016-05-04 10:53:01 +0800

    zj

---
+++ app/Project/Api/Test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 默认接口服务类
+ *
+ * @author: dogstar <chanzonghuang@gmail.com> 2014-10-04
+ */
+
+class Api_Test extends PhalApi_Api {
+	public function index()
+	{
+		return '这是一个测试';
+	}
+}
